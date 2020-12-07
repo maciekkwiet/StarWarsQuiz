c034267c3f828d97d7c6b31afc9b1bfd9552fed4
Magdalena Czajka <magdalena.czajka94@gmail.com>
2020-12-07 20:57:55 +0100

    init

---
+++ src/app/App.js
@@ -0,0 +1,4 @@
+import Box from './components/Box';
+export const App = ({ options }) => {
+  new Box('MODE: who is this character', 'box');
+};
---
+++ src/app/components/Box.js
@@ -0,0 +1,18 @@
+class Box {
+  constructor(content, id) {
+    this.render(content, id);
+  }
+
+  contentRender(content, id) {
+    const container = document.querySelector(`#${id}`);
+    const boxContent = document.createElement('span');
+    const text = container.appendChild(boxContent);
+    text.innerHTML = content;
+  }
+
+  render(content, id) {
+    this.contentRender(content, id);
+  }
+}
+
+export default Box;
