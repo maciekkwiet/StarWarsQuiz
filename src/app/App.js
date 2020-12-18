354551aa69a8cee9b68c909ed5bebdf7a52fe43b
wojaneta <wojaneta@gmail.com>
2020-12-18 22:42:52 +0100

    RankingBox added

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,9 @@
 import Box from './components/Box';
 import Button from './components/Button';
+import { RankingBox } from './components/rankingBox';
 export const App = ({ options }) => {
   new Box('MODE: who is this character', 'box');
   new Button('button', 'play the game');
+  const scores = [{player: "Anna", correctAnswers: 10, allAnswers:20},{player: "Kamil", correctAnswers: 7, allAnswers:20}, {player: "Ela", correctAnswers: 3, allAnswers:20}];
+  new RankingBox('ranking-box', scores );
 };
