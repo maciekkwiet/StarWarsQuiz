92b38b3f3a5509bc78e2b66a6dc17aa7a425845c
tadae <edyta.lucja@gmail.com>
2020-12-21 19:19:22 +0100

    Added Modal import and the class constructor call

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
 import Box from './components/Box';
 import Button from './components/Button';
+import Modal from './components/Modal';
 export const App = ({ options }) => {
   new Box('MODE: who is this character', 'box');
   new Button('button', 'play the game');
+  new Modal('modal', 'content');
 };
