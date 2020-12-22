43933938b6f5a2b7e54d48a702e77c70322627df
wojaneta <wojaneta@gmail.com>
2020-12-22 15:08:01 +0100

    Code clean-up

--- src/app/components/RankingBox.js
+++ src/app/components/RankingBox.js
@@ -1,77 +1,94 @@
-export class RankingBox {
-    constructor(id, scores) {
-      this.render(id, scores);
+export class Player {
+    constructor(onQuestion, onAnswer) {
+     this.onQuestion = onQuestion;
+     this.onAnswer = onAnswer;
+    }
+
+    askQuestion(question){
+        this
+    }
+
+    answerQuestion(){
+
     }
       
     renderContent(id, scores) {
         const rankingBoxContainer = document.querySelector(`#${id}`);
 
-        let rankingHeader = document.createElement('div');
-        rankingBoxContainer.appendChild(rankingHeader);
-        rankingHeader.classList.add("ranking-header");
-        
-        let logoImg = document.createElement('img');
-        logoImg.src = '../../static/assets/img/modes/icons/contacts_24px.png'
-        logoImg.classList.add("ranking-img");
-        rankingHeader.appendChild(logoImg);
+        this.renderHeader(rankingBoxContainer);
 
-        let spanText = document.createElement('span')
-        rankingHeader.appendChild(spanText);
-        spanText.innerHTML = "Mode Ranking";
-
-        let rankingTable = document.createElement('table');
-        rankingBoxContainer.appendChild(rankingTable);
-        rankingTable.classList.add("ranking-table");
-
-        let tableHeaderRow = document.createElement('tr');
-        rankingTable.appendChild(tableHeaderRow);
-
-
-        let tableHeaderFirst = document.createElement('th');
-        tableHeaderRow.appendChild(tableHeaderFirst);
-        tableHeaderFirst.innerHTML = "Place";
-        tableHeaderFirst.classList.add("first-column");
-
-        let tableHeaderSecond = document.createElement('th');
-        tableHeaderRow.appendChild(tableHeaderSecond);
-        tableHeaderSecond.innerHTML = "Player";
-        tableHeaderSecond.classList.add("second-column");
-
-        
-        let tableHeaderThird= document.createElement('th');
-        tableHeaderRow.appendChild(tableHeaderThird);
-        tableHeaderThird.innerHTML = "Answered";
-        tableHeaderThird.classList.add("third-column");
-
-        for(let i=0; i < scores.length; i++) {
-            let tableRow = document.createElement('tr');
-            rankingTable.appendChild(tableRow);
-
-            let tableData1 = document.createElement('td');
-            tableRow.appendChild(tableData1);
-            tableData1.innerHTML = this.numberToOrdinal(i+1);
-            let tableData2 = document.createElement('td');
-            tableRow.appendChild(tableData2);
-            tableData2.innerHTML = scores[i].player;
-            let tableData3 = document.createElement('td');
-            tableRow.appendChild(tableData3);
-            tableData3.innerHTML = scores[i].correctAnswers+"/"+scores[i].allAnswers;
-
-        }
-    
+        this.renderTable(scores, rankingBoxContainer);
     }
 
     render(id, scores) {
         this.renderContent(id, scores);
     }
 
-    numberToOrdinal(number){
-        if (number === 1){
-            return "1st";
-        } else if (number === 2) {
-            return "2nd";
-        } else if (number === 3) {
-            return "3rd";
+    numberToOrdinal(number) { 
+        switch (number) {
+            case 1:
+                return "1st";
+            case 2:
+                return "2nd";
+            case 3:
+                return "3rd";
+        }
+    }
+
+    renderHeader(rankingBoxContainer) {
+        const rankingHeader = document.createElement('div');
+        rankingBoxContainer.appendChild(rankingHeader);
+        rankingHeader.classList.add("ranking-header");
+        
+        const logoImg = document.createElement('img');
+        logoImg.src = '../../static/assets/img/modes/icons/contacts_24px.png'
+        logoImg.classList.add("ranking-img");
+        rankingHeader.appendChild(logoImg);
+
+        const spanText = document.createElement('span')
+        rankingHeader.appendChild(spanText);
+        spanText.innerHTML = "Mode Ranking";
+    }
+
+    renderTable(scores, rankingBoxContainer) {
+        const rankingTable = document.createElement('table');
+        rankingBoxContainer.appendChild(rankingTable);
+        rankingTable.classList.add("ranking-table");
+
+        const tableHeaderRow = document.createElement('tr');
+        rankingTable.appendChild(tableHeaderRow);
+
+        const tableHeaderFirst = document.createElement('th');
+        tableHeaderRow.appendChild(tableHeaderFirst);
+        tableHeaderFirst.innerHTML = "Place";
+        tableHeaderFirst.classList.add("first-column");
+
+        const tableHeaderSecond = document.createElement('th');
+        tableHeaderRow.appendChild(tableHeaderSecond);
+        tableHeaderSecond.innerHTML = "Player";
+        tableHeaderSecond.classList.add("second-column");
+        
+        const tableHeaderThird= document.createElement('th');
+        tableHeaderRow.appendChild(tableHeaderThird);
+        tableHeaderThird.innerHTML = "Answered";
+        tableHeaderThird.classList.add("third-column");
+
+        for (let i=0; i < scores.length; i++) {
+            const tableRow = document.createElement('tr');
+            rankingTable.appendChild(tableRow);
+
+            const tableData1 = document.createElement('td');
+            tableRow.appendChild(tableData1);
+
+            tableData1.innerHTML = this.numberToOrdinal(i+1);
+            const tableData2 = document.createElement('td');
+            tableRow.appendChild(tableData2);
+
+            tableData2.innerHTML = scores[i].player;
+            const tableData3 = document.createElement('td');
+
+            tableRow.appendChild(tableData3);
+            tableData3.innerHTML = scores[i].correctAnswers+"/"+scores[i].allAnswers;
         }
     }
 }
