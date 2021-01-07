a2576bcbb39ed04acdafc485c316d2a1ba52c6b3
Wojtek Warmuz <wwarmuz@gmail.com>
2021-01-07 20:16:47 +0100

    Add reload to Logo

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -6,11 +6,14 @@ class Logo {
   contentRender(id) {
     const container = document.querySelector(`#${id}`);
     const pictureSrc = '../static/assets/ui/StarWarsLogo.png';
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', 'javascript:location.reload()');
     const picture = document.createElement('img');
     container.classList.add('logo');
     picture.setAttribute('src', pictureSrc);
     picture.setAttribute('alt', 'Star Wars Logo');
-    container.appendChild(picture);
+    container.appendChild(anchor);
+    anchor.appendChild(picture);
   }
 
   render(id) {
