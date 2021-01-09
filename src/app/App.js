b364f6d5cb6aa7fdda95190d6996c37d4e453a95
wojaneta <wojaneta@gmail.com>
2021-01-09 09:50:14 +0100

    Code cleaned up

--- src/app/App.js
+++ src/app/App.js
@@ -6,6 +6,7 @@ import WhiteButton from './components/WhiteButton';
 import Modal from './components/Modal';
 import Timer from './components/Timer';
 import Rules from './components/Rules';
+import GameOverScreen from './components/ModalContent';
 class App {
   constructor(options) {
     this.box = new Box('MODE: who is this character', 'box');
@@ -13,6 +14,30 @@ class App {
     const scores = [{player: "Anna", correctAnswers: 10, allAnswers:20},{player: "Kamil", correctAnswers: 7, allAnswers:20}, {player: "Ela", correctAnswers: 3, allAnswers:20}];
     this.rankingBox = new RankingBox('ranking-box', scores );
     this.modal = new Modal('modalBox');
+
+    const answers = [{
+      questionPicture: "../../static/assets/img/modes/people/1.jpg",
+      playerAnswer: "DarthVader",
+      computerAnswer: "Darth Father",
+      correctAnswer: "Luke Skywalker",
+      playerAnswerIsCorrect: false,
+      computerAnswerIsCorrect: false,
+    },
+    {
+      questionPicture: '../../static/assets/img/modes/people/36.jpg',
+      playerAnswer: "Jar Jar Binks",
+      computerAnswer: "Jar Jar Binks",
+      correctAnswer: "Jar Jar Binks",
+      playerAnswerIsCorrect: true,
+      computerAnswerIsCorrect: true,
+    },
+    ];
+    const closeWindow = (name, punctation) => {
+      console.log(name, punctation);
+      this.modal.closeModal();
+    };
+    this.gameOverScreen = new GameOverScreen(answers, closeWindow, 'modalBox');
+    
     this.mainMenuPanel = new MainMenu('mainMenu', ['People', 'Vehicles', 'Starships'], 0);
     this.whiteButton = new WhiteButton('whiteButton', 'Hall of fame', 'whiteButton');
     this.whiteButton.addIcon('../../static/assets/ui/hof.svg');
--- src/app/components/ModalContent.js
+++ src/app/components/ModalContent.js
@@ -1,9 +1,25 @@
 export default class GameOverScreen {
-    constructor(answers, id) {
-      this.render(answers, id);
+    constructor(answers,closeWindow, id) {
+      this.render(answers,closeWindow, id);
     }
   
-    contentRender(answers, id) {
+    contentRender(answers,closeWindow, id) {
+
+      const playerCorrectAnswers = answers.reduce(function(a,b) {
+        if (b.playerAnswerIsCorrect === true) {
+          return a + 1;
+        } else {
+          return a;
+        }
+      }, 0);
+      const computerCorrectAnswers = answers.reduce(function(a,b) {
+        if (b.computerAnswerIsCorrect === true) {
+          return a + 1;
+        } else {
+          return a;
+        }
+      }, 0);
+
       const modalBox = document.querySelector(`#${id}`);
       const gameOverCaption= document.createElement('span');
       const text = modalBox.appendChild(gameOverCaption);
@@ -12,12 +28,12 @@ export default class GameOverScreen {
     
       const finalComment= document.createElement('p');
       const comment = modalBox.appendChild(finalComment);
-      comment.innerHTML = checkWhoWon(answers);
+      comment.innerHTML = this.checkWhoWon(playerCorrectAnswers, computerCorrectAnswers, answers);
       comment.classList.add("final-comment");
 
       const tableName= document.createElement('p');
       const name = modalBox.appendChild(tableName);
-      name.innerHTML = "Detailed answers";
+      name.innerHTML = "Detailed answers:";
       name.classList.add("table-name");
     
       const summary= document.createElement('div');
@@ -28,36 +44,114 @@ export default class GameOverScreen {
       picture.src = "../../static/assets/img/modes/MasterYoda.png";
       const YodaImg = summary.appendChild(picture);
       
+      const table= document.createElement('div');
+      const tableContent = summary.appendChild(table);
+      tableContent.classList.add("grid-table");
+
+      const tHeaderColumn1= document.createElement('span');
+      const tHeaderColumn1Content= table.appendChild(tHeaderColumn1);
+      tHeaderColumn1Content.classList.add("table-header");
+
+      const tHeaderColumn2= document.createElement('span');
+      const tHeaderColumn2Content= table.appendChild(tHeaderColumn2);
+      tHeaderColumn2Content.innerHTML = "You";
+      tHeaderColumn2Content.classList.add("table-header");
+
+      const tHeaderColumn3= document.createElement('span');
+      const tHeaderColumn3Content= table.appendChild(tHeaderColumn3);
+      tHeaderColumn3Content.innerHTML = "Computer";
+      tHeaderColumn3Content.classList.add("table-header");
+
+      const tHeaderColumn4= document.createElement('span');
+      const tHeaderColumn4Content= table.appendChild(tHeaderColumn4);
+      tHeaderColumn4Content.innerHTML = "Answer";
+      tHeaderColumn4Content.classList.add("table-header");
 
 
+      for (let i = 0; i < answers.length; i++) {
+        const tableRowColumn1 = document.createElement('span');
+        table.appendChild(tableRowColumn1);
+
+        const question = document.createElement('img');
+        question.src = answers[i].questionPicture;
+        tableRowColumn1.appendChild(question);
+        question.classList.add("img-row")
+
+        const tableRowColumn2 = document.createElement('span');
+        const playerAnswer = table.appendChild(tableRowColumn2);
+        playerAnswer.innerHTML = answers[i].playerAnswer
+        
+        const tableRowColumn3 = document.createElement('span');
+        const computerAnswer = table.appendChild(tableRowColumn3);
+        computerAnswer.innerHTML = answers[i].computerAnswer
+
+        if (answers[i].playerAnswerIsCorrect === true) {
+          playerAnswer.classList.add("correct-answer");
+        } else {
+          playerAnswer.classList.add("incorrect-answer");
+        }
+
+        const tableRowColumn4 = document.createElement('span');
+        const correctAnswer = table.appendChild(tableRowColumn4);
+        correctAnswer.innerHTML = answers[i].correctAnswer
+
+      
+        if (answers[i].computerAnswerIsCorrect === true) {
+          computerAnswer.classList.add("correct-answer");
+        } else {
+          computerAnswer.classList.add("incorrect-answer");
+        }
+      }
+      const formContainer= document.createElement('div');
+      const formDiv = modalBox.appendChild(formContainer);
+      formDiv.classList.add("form-name");
+
+      const form= document.createElement('form');
+      const formWindow = formContainer.appendChild(form);
+      formWindow.classList.add("form-box")
+
+      const input =  document.createElement('input');
+      const inputArea = form.appendChild(input);
+      inputArea.classList.add("input-window")
+      inputArea.setAttribute('type', 'text')
+      inputArea.setAttribute('id', 'inputContent');
+      inputArea.setAttribute('required','');
+      inputArea.required = true;
+      
+
+      const description= document.createElement('p');
+      const formDescription = formContainer.appendChild(description);
+      formDescription.innerHTML = 'Please fill your name in order to receive eternal glory in whole Galaxy!'
+      formDescription.classList.add("form-description");
+    
+      const button= document.createElement('button');
+      const modalButton = modalBox.appendChild(button);
+      modalButton.innerHTML = "may the force be with you";
+      modalButton.classList.add("modal-button");
+      
+      const playerScore = `${playerCorrectAnswers}/${(answers.length)}`;
+
+      modalButton.addEventListener("click",function(){
+      if (document.querySelector("form").checkValidity() === true) {
+        closeWindow(input.value, playerScore);
+      } else {
+        document.querySelector("form").reportValidity()}
+      });
 
     }
   
-    render(answers, id) {
-      this.contentRender(answers, id);
+    render(answers,closeWindow, id) {
+      this.contentRender(answers, closeWindow, id);
+    }
+
+    checkWhoWon (player1Answers,player2Answers, allAnswers) {
+
+      if (player1Answers > player2Answers) {
+        return `The force is strong in you young Padawan! During 1 minute you have answered ${player1Answers}/${allAnswers.length} questions. And Computer quessed ${player2Answers}/${allAnswers.length}.`
+      } else if (player1Answers < player2Answers) {
+        return `The force is strong in Computer! During 1 minute Computer quessed ${player2Answers}/${allAnswers.length} questions. And You ${player1Answers}/${allAnswers.length}.`
+      } else {
+        return `During 1 minute you and Computer quessed ${player2Answers}/${allAnswers.length} questions.`
+      }
     }
   }
-
-function checkWhoWon (answers) {
-    let correctPlayerAnswers = [];
-    let correctComputerAnswers = [];
-            for (let i=0; i < answers.length; i++) {
-    
-            if (answers[i].playerAnswer === answers[i].correctAnswer) {
-                correctPlayerAnswers.push(answers[i].playerAnswer)
-            }
-            }
-            for (let i=0; i < answers.length; i++) {
-        
-            if (answers[i].computerAnswer === answers[i].correctAnswer) {
-                correctComputerAnswers.push(answers[i].ComputerAnswer)
-            }
-             }
-            if (correctPlayerAnswers > correctComputerAnswers) {
-                return `The force is strong in you young Padawan! During 1 minute you have answered ${correctPlayerAnswers.length}/${answers.length} questions. And Computer quessed ${correctComputerAnswers.length}/${answers.length}.`
-            } else if (correctPlayerAnswers < correctComputerAnswers) {
-                return `The force is strong in Computer! During 1 minute Computer quessed ${correctComputerAnswers.length}/${answers.length} questions. And You ${correctPlayerAnswers.length}/${answers.length}.`
-            } else {
-                return `During 1 minute you and Computer quessed ${correctComputerAnswers.length}/${answers.length} questions.`
-            }
-}
