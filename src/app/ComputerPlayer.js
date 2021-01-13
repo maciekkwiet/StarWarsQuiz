15d5fd0695683cbf621179bc7819a0fc664a0ae3
wojaneta <wojaneta@gmail.com>
2021-01-13 19:49:01 +0100

    ComputerPlayer.js code changes after review

--- src/app/ComputerPlayer.js
+++ src/app/ComputerPlayer.js
@@ -1,9 +1,9 @@
-export class ComputerPlayer {
+class ComputerPlayer {
     constructor(answerSelectedCallback) {
         this.answerSelectedCallback = answerSelectedCallback;
     }
     askQuestion(answersList) {
-        const selectedAnswerIndex = Math.floor(Math.random() * 4);
+        const selectedAnswerIndex = Math.floor(Math.random() * answersList.length);
         const selectedAnswer  = answersList[selectedAnswerIndex];
         this.answerSelectedCallback(selectedAnswer);
     }
