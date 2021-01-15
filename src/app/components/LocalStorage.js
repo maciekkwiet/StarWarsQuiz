d373d79d7e1e4eac63590cb58d34a1b85839b605
Wojtek Warmuz <wwarmuz@gmail.com>
2021-01-15 21:42:20 +0100

    refactor

--- src/app/components/LocalStorage.js
+++ src/app/components/LocalStorage.js
@@ -1,12 +1,18 @@
 const compareNumbers = (a,b)=>b.score-a.score;
 
-export const LocalStorageRanking=(gameModeIndex,playerCorrectAnswers,playerAllAnswers,playerName)=>{
-
-  const savedScores = JSON.parse(localStorage.getItem(gameModeIndex))||[];
-  let newPlayer = {name:playerName, correctAnswers:playerCorrectAnswers,allAnswers:playerAllAnswers };
-  savedScores.push(newPlayer);
-  savedScores.forEach(el => el.score = el.correctAnswers/el.allAnswers);
-  let newHighScores =savedScores.sort(compareNumbers);
-  let scoresToPush = newHighScores.splice(0,3);
-  localStorage.setItem(gameModeIndex,JSON.stringify(scoresToPush));
-}
+export const GetLocatStorage=(gameModeIndex)=>JSON.parse(localStorage.getItem(gameModeIndex))||[];
+export const SetLocatStorage=(gameModeIndex,playerName,playerCorrectAnswers,playerAllAnswers)=>{
+  if(ScoreCheck(gameModeIndex)){
+    const actualLocalStorage = GetLocatStorage(gameModeIndex).slice(0,2);
+    let newPlayer = {name:playerName, correctAnswers:playerCorrectAnswers,allAnswers:playerAllAnswers}
+    actualLocalStorage.push(newPlayer);
+    actualLocalStorage.forEach(el => el.score = el.correctAnswers/el.allAnswers);
+    let newHighScores =actualLocalStorage.sort(compareNumbers);
+    localStorage.setItem(gameModeIndex,JSON.stringify(newHighScores));
+  }
+};
+export const ScoreCheck=(gameModeIndex,playerCorrectAnswers,playerAllAnswers)=>{
+  const actualLocalStorage = GetLocatStorage(gameModeIndex);
+  const lastScores =actualLocalStorage[actualLocalStorage.lenght-1];
+  return playerCorrectAnswers/playerAllAnswers>lastScores.correctAnswers/lastScores.allAnswers;
+};
